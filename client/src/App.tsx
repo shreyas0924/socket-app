638556c06f16d0a2f3f3d78dff4f3b796a1a92c0
shreyas0924 <shreyasp0924@gmail.com>
2023-07-23 10:14:06 +0530

    added shadcn ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-  return <>Google Docs</>
+  return (
+    <>
+      <Button>Hello</Button>
+    </>
+  )
 }
 
 export default App
