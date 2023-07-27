e01550e22b06d48d4e6da7c8aead25f5c0a9cace
shreyas0924 <shreyasp0924@gmail.com>
2023-07-27 22:55:08 +0530

    finally connected socket to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import Navbar from './components/Navbar'
-
+import {io} from "socket.io-client"
 import { Textarea } from './components/ui/textarea'
-
+const socket = io("http://localhost:3001/")
 function App() {
+  socket.on("connection", () => { console.log("server connected") })
   return (
     <>
       <Navbar />
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,21 @@ import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
 
-const port: number = 3001
+const port: number = 3000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World')
+  res.send('Hello World')
 })
 
 app.listen(port, function () {
-    console.log(`App is listening on port ${port} !`)
+  console.log(`App is listening on port ${port} !`)
+})
+
+const http = require('http').Server(app)
+
+const server = http.listen(3001, function () {
+  console.log('server listening on *:3001')
+})
+server.on("connection" , () => {
+    console.log("client connected")
 })
