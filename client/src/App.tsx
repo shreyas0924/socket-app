630a7ebd0f0f06fa18b842de556cfe495d51da2a
shreyas0924 <shreyasp0924@gmail.com>
2023-07-30 21:37:31 +0530

    sockets not working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import io from 'socket.io-client'
 import Navbar from './components/Navbar'
-import {io} from "socket.io-client"
 import { Textarea } from './components/ui/textarea'
-const socket = io("http://localhost:3001/")
+
+const socket = io('http://127.0.0.1:4000')
+
 function App() {
-  socket.on("connection", () => { console.log("server connected") })
+  const [text, setText] = useState<string>('')
+
+  useEffect(() => {
+
+    socket.on("connection", () => console.log("Connected to Socket"))
+    socket.on('textChange', (newText: string) => {
+      setText(newText)
+    })
+
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value
+    setText(newText)
+    socket.emit('textChange', newText)
+  }
+
   return (
     <>
       <Navbar />
       <div className='flex justify-center'>
-        <Textarea className='w-1/2' />
+        <Textarea className='w-1/2' value={text} onChange={handleTextChange} />
       </div>
     </>
   )
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,32 @@
 import express, { Application, Request, Response } from 'express'
+import { createServer } from 'http'
+import { Server, Socket } from 'socket.io'
+import cors from 'cors'
 
 const app: Application = express()
+const port: number = 4000
 
-const port: number = 3000
+app.use(cors()) // Enable CORS for all routes
+
+const httpServer = createServer(app)
+const io = new Server(httpServer)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World')
 })
 
-app.listen(port, function () {
-  console.log(`App is listening on port ${port} !`)
+io.on('connection', (socket: Socket) => {
+  console.log('A client connected!')
+
+  socket.on('textChange', (text: string) => {
+    io.emit('textChange', text)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('A client disconnected!')
+  })
 })
 
-const http = require('http').Server(app)
-
-const server = http.listen(3001, function () {
-  console.log('server listening on *:3001')
-})
-server.on("connection" , () => {
-    console.log("client connected")
+httpServer.listen(port, () => {
+  console.log(`App is listening on port ${port}!`)
 })
