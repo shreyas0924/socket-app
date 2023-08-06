dd4ce3b95fca7e7bce988355536aa9678a70a262
shreyas0924 <shreyasp0924@gmail.com>
2023-08-06 14:07:54 +0530

    socket working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react'
 import io from 'socket.io-client'
 import Navbar from './components/Navbar'
 import { Textarea } from './components/ui/textarea'
 
-const socket = io('http://127.0.0.1:4000')
+const socket = io('http://localhost:3000')
 
 function App() {
   const [text, setText] = useState<string>('')
 
   useEffect(() => {
+    console.log('Sending doc:', text)
+    socket.emit('send-doc', { text })
 
-    socket.on("connection", () => console.log("Connected to Socket"))
-    socket.on('textChange', (newText: string) => {
-      setText(newText)
+    socket.on('receive-doc', (e) => {
+      console.log('Received doc:', e.text)
+      setText(e.text)
     })
 
     return () => {
-      socket.disconnect()
+      console.log('Cleaning up')
+      socket.off('receive-doc')
     }
   }, [])
 
   const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const newText = event.target.value
     setText(newText)
-    socket.emit('textChange', newText)
+
+    socket.emit('send-doc', { text: newText })
   }
 
   return (
@@ -31,8 +36,7 @@ function App() {
       <Navbar />
       <div className='flex justify-center'>
         <Textarea className='w-1/2' value={text} onChange={handleTextChange} />
-        <Textarea className='w-1/2' value={text} onChange={handleTextChange} />
-        
+        {/* <Textarea className='w-1/2' value={text} onChange={handleTextChange} /> */}
       </div>
     </>
   )
